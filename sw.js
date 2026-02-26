9f51bc79186bb88d05ee90fc03e85043dff9db29
edcpc01 <ed.cpc01@gmail.com>
2026-02-26 14:43:37 -0300

    Update cache version in service worker

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // Regra de ouro: NUNCA intercepta requisicoes externas (Firebase, gstatic, etc.)
 // Apenas arquivos do proprio dominio sao cacheados.
 
-const CACHE = 'mp-v6efdaf94';
+const CACHE = 'mp-v6efdaf95';
 
 // Install: cacheia os arquivos do app para funcionar offline
 self.addEventListener('install', e => {
